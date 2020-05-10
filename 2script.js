ed7cc7e3c456b5e1a1cee09aa49e61e162a22173
Ilya <slom20252025@gmail.com>
2020-05-10 20:36:22 +0300

    course update,second homeWork

--- 2script.js
+++ 2script.js
@@ -10,13 +10,54 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-
-let a = prompt("Один из последних просмотренных фильмов?"),
-    b = prompt("На сколько оцените его?"),
-    c = prompt("Один из последних просмотренных фильмов?"),
-    d = prompt("На сколько оцените его?");
+ 
 
 
-//personalMovieDB.count[numberOfFilms];
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+       let a = prompt("Один из последних просмотренных фильмов?", ''),
+           b = prompt("На сколько оцените его?", '');
+    
+    personalMovieDB.movies[a] = b;
+
+        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+    
+    } else {
+        i--;
+        console.log("Я дебил");
+    }
+};
+
+/*let i = 0;
+
+while (i < 2) {
+   let a = prompt("Один из последних просмотренных фильмов?", ''),
+       b = prompt("На сколько оцените его?", '');
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+    i++;
+    continue;
+    } 
+    else {
+        i--;
+        console.log("Введите ещё раз");
+    }
+};*/
+
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log("мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log("Средне");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("error");
+};
+
+
+
+
+
